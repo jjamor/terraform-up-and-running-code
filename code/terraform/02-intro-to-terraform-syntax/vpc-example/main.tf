f3a583ba23e1803d168765b5ffefb9e982ed5ee7
Juanjo Amor <jjamorNOSPAM@gmail.com>
2018-04-11 17:01:24 +0200

    example: vpc creation

---
+++ code/terraform/02-intro-to-terraform-syntax/vpc-example/main.tf
@@ -0,0 +1,54 @@
+# Configure the AWS Provider
+provider "aws" {
+  region     = "us-east-1"
+}
+
+data "aws_availability_zones" "azs" {
+}
+
+resource "aws_vpc" "tfvpctest" {
+ cidr_block = "172.32.0.0/16"
+ tags {
+   Name = "tfvpctest"
+ }
+}
+
+resource "aws_subnet" "tfsubnet1" {
+ vpc_id = "${aws_vpc.tfvpctest.id}"
+ cidr_block = "172.32.0.0/24"
+ availability_zone = "${data.aws_availability_zones.azs.names[0]}"
+ tags {
+   Name = "tfsubnet1"
+ }
+}
+
+resource "aws_subnet" "tfsubnet2" {
+ vpc_id = "${aws_vpc.tfvpctest.id}"
+ cidr_block = "172.32.1.0/24"
+ availability_zone = "${data.aws_availability_zones.azs.names[1]}"
+ tags {
+   Name = "tfsubnet2"
+ }
+}
+
+resource "aws_subnet" "tfsubnet3" {
+ vpc_id = "${aws_vpc.tfvpctest.id}"
+ cidr_block = "172.32.2.0/24"
+ availability_zone = "${data.aws_availability_zones.azs.names[2]}"
+ tags {
+   Name = "tfsubnet3"
+ }
+}
+
+resource "aws_internet_gateway" "tfgw" {
+  vpc_id = "${aws_vpc.tfvpctest.id}"
+
+  tags {
+    Name = "tfgw"
+  }
+}
+
+#output "outputazs" {
+#  value = "${data.aws_availability_zones.azs.names}"
+#}
+
